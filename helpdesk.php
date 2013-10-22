1516b601b3949cbde3347799927cd83d86e821f0
maxt3r <maxt3r@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-22 21:24:12 +0000

    init
    git-svn-id: https://plugins.svn.wordpress.org/jitbit-helpdesk/trunk@792037 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ helpdesk.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @package Jitbit_Helpdesk
+ * @version 0.1
+ */
+/*
+Plugin Name: Jitbit Help Desk for Wordpress
+Plugin URI: http://www.jitbit.com/hosted-helpdesk/
+Description: This plugin inserts Jitbit Help Desk widget on every page of your Wordpress blog.
+Author: Jitbit
+Version: 0.1
+Author URI: http://jitbit.com/
+*/
+
+
+// This just echoes the chosen line, we'll position it later
+function get_widget() {
+	$jitbit_hd_url = rtrim(get_option("jitbit_hd_url"), "/");
+    if(!empty($jitbit_hd_url)){
+    $path = "/js/support-widget.js";
+		echo "<script type='text/javascript' src='".$jitbit_hd_url.$path."'></script>
+        <script type='text/javascript'>
+            CreateSupportWidget('left');
+        </script>";
+	}
+}
+
+// Now we set that function up to execute when the admin_notices action is called
+add_action( 'wp_footer', 'get_widget' );
+
+
+function setup_admin_menus() {
+      add_submenu_page('plugins.php',
+        'Helpdesk Settings', 'Jitbit Helpdesk', 'manage_options',
+        'hd-elements', 'hd_settings');
+}
+
+function hd_settings() {
+	if (!current_user_can('manage_options')) {
+    wp_die('You do not have sufficient permissions to access this page.');
+	}
+
+	if (isset($_POST["update_settings"])) {
+    // Do the saving
+    $url = esc_attr($_POST["url"]);
+		update_option("jitbit_hd_url", $url);
+		?>
+    <div id="message" class="updated">Settings saved</div>
+		<?php
+	}
+
+	$jitbit_hd_url = get_option("jitbit_hd_url");
+ ?>
+ <div class="wrap">
+        <?php screen_icon('plugins'); ?> <h2>Jitbit Help Desk Settings</h2>
+
+        <form method="POST" action="">
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">
+                            <label for="url">
+												        Your Helpdesk URL:
+												    </label>
+                    </th>
+                    <td>
+                            <input type="text" name="url" value="<?php echo $jitbit_hd_url;?>" placeholder="https://support.jitbit.com/helpdesk">
+                            <p class="description">Make sure you enter the full URL like this https://%your_company_name%.jitbit.com<b>/helpdesk</b>"</p>
+                            <input type="hidden" name="update_settings" value="Y" />
+                    </td>
+                </tr>
+            </table>
+            <p>
+					    <input type="submit" value="Save settings" class="button-primary"/>
+						</p>
+        </form>
+    </div>
+
+
+
+<?php
+}
+// This tells WordPress to call the function named "setup_admin_menus"
+// when it's time to create the menu pages.
+add_action("admin_menu", "setup_admin_menus");
+?>
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Jitbit Help Desk for Wordpress ===
+Contributors: maxt3r
+Tags: helpdesk, widget
+Requires at least: 3.0
+Tested up to: 3.6.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds Jitbit Help Desk widget to every page of your Wordpress blog.
+
+== Description ==
+
+To use this plugin you need to have a [Jitbit Help Desk](http://www.jitbit.com/hosted-helpdesk/) account. You can register for a free trial here: http://www.jitbit.com/hosted-helpdesk/trial/ It takes 30 seconds and does not require a credit card.
+
+After the registration, you will get your own Help Desk URL (https://%your_company_name%.jitbit.com/helpdesk), you will need it to make this plugin work.
+
+Jitbit Help Desk for Wordpress loads an external javascript file from Jitbit's servers. It will execute in your visitors browsers, but don't worry, the only purpose of the plugin is to create a Help Desk widget.
+
+Also please note that all communication between your Wordpress site and Jitbit servers is secured with SSL.
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the "Jitbit Help Desk" menu and enter your full Help Desk URL like https://chat.jitbit.com/helpdesk. Do not forget to include /helpdesk on the end.
+
+That's it. Now you should see the Help Desk widget on the  left side of every public page of your web-site.
+
+== Changelog ==
+
+= 0.1 =
+* The first version
